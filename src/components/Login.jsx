f2295624b981579bdb510bf63d4bd5dbdc5eca18
Nancy.D <Nancy.D@ripplr.in>
2022-05-23 16:05:41 +0530

    login form.item added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,33 +2,34 @@ import React from "react";
 import { Formik } from "formik";
 import { Form, Input, SubmitButton, ResetButton } from "formik-antd";
 import * as Yup from "yup";
-import { Space, message } from "antd";
+import { message, Row, Col } from "antd";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { login } from "../redux/actionType";
 import "../Home.css";
+import "../TaskStyle.css";
 
 function Login() {
   const dispatch = useDispatch();
   let history = useHistory();
 
   const initialValues = {
-    email: "",
-    pwd: "",
+    email: undefined,
+    pwd: undefined,
   };
   const validateSchema = Yup.object().shape({
-    email: Yup.string().required("Required").email("Invalid Email"),
-    pwd: Yup.string().required("Required"),
+    email: Yup.string().required("Email Required").email("Invalid Email"),
+    pwd: Yup.string().required("Password Required"),
   });
 
   function onSubmit(values) {
-    const userData ={
+    const userData = {
       email: values.email,
-      password: values.pwd
-    }
+      password: values.pwd,
+    };
     dispatch(login(userData));
     message.success({ content: "Logged In", duration: 2 });
-    history.push('/taskredux');
+    history.push("/taskredux");
   }
 
   return (
@@ -38,35 +39,56 @@ function Login() {
       validationSchema={validateSchema}
     >
       {(props) => (
-        <div className="form">
-          <Form className="form">
-          <Input
-            type="email"
-            id="text"
-            placeholder="Email"
-            name="email"
-            value={props.values.email}
-            onChange={props.handleChange}
-            onBlur={props.handleBlur}
-            validate={props.errors.email?"error":"success"}
-          />
-          {props.touched.email && props.errors.email ? <div className="red">{props.errors.email}</div>:null}
-          <Input
-            type='password'
-            placeholder="Password"
-            id='pwd'
-            name="pwd"
-            value={props.values.pwd}
-            onChange={props.handleChange}
-            onBlur={props.handleBlur}
-          />
-          {props.touched.pwd && props.errors.pwd ? <div className="red">{props.errors.pwd}</div>:null}
-          <Space direction="horizontal">
-          <SubmitButton className="btn">Submit</SubmitButton>
-          <ResetButton className="btn">Reset</ResetButton>
-          </Space>
+        <Form
+          style={{
+            height: "100%",
+            margin: "40px 80px",
+            padding: "20px 0",
+          }}
+        >
+          <Row>
+            <Col span={24}>
+              <Form.Item
+                name="email"
+                label="Email"
+                labelCol={{ span: 6 }}
+                wrapperCol={{ span: 12 }}
+              >
+                <Input
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  value={props.values.email}
+                  onChange={props.handleChange}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col span={24}>
+              <Form.Item
+                name="pwd"
+                label="Password"
+                labelCol={{ span: 6 }}
+                wrapperCol={{ span: 12 }}
+              >
+                <Input
+                  type="password"
+                  placeholder="Password"
+                  name="pwd"
+                  size="small"
+                  onChange={props.handleChange}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+
+          <div className="div-btn">
+            <SubmitButton className="btn">Submit</SubmitButton>
+            <ResetButton className="btn">Reset</ResetButton>
+          </div>
         </Form>
-        </div>
       )}
     </Formik>
   );
