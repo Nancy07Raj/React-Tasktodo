c6e45b238ab94fe896f7eda4215b99bcd5780011
Nancy <nancyd0716@gmail.com>
2022-05-26 11:20:25 +0530

    regForm upload added

---
+++ src/components/Debug.js
@@ -0,0 +1,59 @@
+import { FormikConsumer } from "formik";
+import React from "react";
+import PropTypes from "prop-types";
+
+function Debug({ show = true }) {
+  if (process.env.NODE_ENV === "production") {
+    return null;
+  }
+  if (show === true) {
+    return (
+      <div
+        style={{
+          margin: "3rem 0",
+          borderRadius: 4,
+          background: "#f6f8fa",
+          boxShadow: "0 0 1px  #eee inset",
+        }}
+      >
+        <div
+          style={{
+            textTransform: "uppercase",
+            fontSize: 11,
+            borderTopLeftRadius: 4,
+            borderTopRightRadius: 4,
+            fontWeight: 500,
+            padding: ".5rem",
+            background: "#555",
+            color: "#fff",
+            letterSpacing: "1px",
+          }}
+        >
+          Formik State
+        </div>
+        <FormikConsumer>
+          {({ ...rest }) => (
+            <pre
+              style={{
+                fontSize: ".65rem",
+                padding: ".25rem .5rem",
+                overflowX: "scroll",
+              }}
+            >
+              {JSON.stringify(rest, null, 2)}
+            </pre>
+          )}
+        </FormikConsumer>
+      </div>
+    );
+  }
+  return null;
+}
+
+Debug.propTypes = {
+  show: PropTypes.bool,
+};
+
+Debug.defaultProps = { show: true };
+
+export { Debug };
--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { Formik } from "formik";
-import { Row, Col } from "antd";
-import { Form, Input, Radio, Select, DatePicker } from "formik-antd";
+import { Row, Col, Upload } from "antd";
+import moment from "moment";
+import {
+  Form,
+  Input,
+  Radio,
+  Select,
+  DatePicker,
+  SubmitButton,
+} from "formik-antd";
 import * as yup from "yup";
+import { Debug } from "./Debug";
 
 const phoneRegex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 
@@ -20,6 +29,7 @@ const init = {
   address: undefined,
   gender: undefined,
   degree: undefined,
+  excel: undefined,
 };
 
 const handleSubmit = (values, { resetForm, setSubmitting }) => {
@@ -35,11 +45,42 @@ const validate = yup.object({
     .string()
     .required("Phone no Required")
     .matches(phoneRegex, "Invalid number"),
-  dob: yup.string().required("DOB required"),
+  dob: yup
+    .string()
+    .required("DOB is Required")
+    .test("DOB", "Please choose a valid date of birth", (value) => {
+      return moment().diff(moment(value), "years") >= 18;
+    }),
   address: yup.string().required("Address Required"),
   gender: yup.number().required("Gender Required"),
   degree: yup.string().required("Degree required"),
+  excel: yup.array().required().label("Excel"),
 });
+
+// this dummy request is to override antd upload component action request
+const dummyRequest = ({ onSuccess }) => {
+  setTimeout(() => {
+    onSuccess("ok");
+  }, 0);
+};
+
+const handleupload = ({ file }, setFieldValue) => {
+  console.log({ file });
+  if (file.status === "uploading" || file.status === "done") {
+    return setFieldValue("excel", [file]);
+  }
+  return setFieldValue("excel", []);
+};
+
+//while handling file formData used to send file value
+
+// const handleSubmit = (values, { resetForm, setSubmitting }) => {
+//   const formData = new FormData();
+//   formData.append('fc', values.fc);
+//   formData.append('brand', values?.brand);
+//   formData.append('excel', values.excel[0]?.originFileObj);
+//   formData.append('fileType', uploadType);
+// }
 
 export default function RegistrationForm() {
   return (
@@ -48,45 +89,117 @@ export default function RegistrationForm() {
       onSubmit={handleSubmit}
       validationSchema={validate}
     >
-      <Form>
-        <Row>
-          <Col>
-            <Form.Item name="name" label="Name">
-              <Input name="name" type="text" />
-            </Form.Item>
-          </Col>
-          <Col>
-            <Form.Item name="email" label="Email">
-              <Input name="email" type="text" />
-            </Form.Item>
-          </Col>
-        </Row>
+      {({ setFieldValue }) => {
+        return (
+          <Form>
+            <Row gutter={16}>
+              <Col sm={8}>
+                <Form.Item
+                  name="name"
+                  label="Name"
+                  labelCol={{ span: 6 }}
+                  wrapperCol={{ span: 16 }}
+                >
+                  <Input name="name" type="text" />
+                </Form.Item>
+              </Col>
+              <Col sm={8}>
+                <Form.Item
+                  name="email"
+                  label="Email"
+                  labelCol={{ span: 6 }}
+                  wrapperCol={{ span: 16 }}
+                >
+                  <Input name="email" type="text" />
+                </Form.Item>
+              </Col>
+            </Row>
 
-        <Form.Item name="phno" label="Phone No">
-          <Input name="phno" type="text" />
-        </Form.Item>
-        <Form.Item name="dob" label="DOB">
-          <DatePicker name="dob" />
-        </Form.Item>
-        <Form.Item name="gender" label="Gender">
-          <Radio.Group name="gender">
-            <Radio value={1}>Male</Radio>
-            <Radio value={2}>Female</Radio>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item name="address" label="Address">
-          <Input name="address" type="text" />
-        </Form.Item>
-        <Form.Item name="degree" label="Degree">
-          <Select
-            style={{
-              width: 120,
-            }}
-            name="degree"
-            options={degreeOption}
-          ></Select>
-        </Form.Item>
-      </Form>
+            <Row>
+              <Col sm={8}>
+                <Form.Item
+                  name="dob"
+                  label="DOB"
+                  labelCol={{ span: 6 }}
+                  wrapperCol={{ span: 16 }}
+                >
+                  <DatePicker name="dob" />
+                </Form.Item>
+              </Col>
+              <Col sm={8}>
+                <Form.Item
+                  name="gender"
+                  label="Gender"
+                  labelCol={{ span: 6 }}
+                  wrapperCol={{ span: 16 }}
+                >
+                  <Radio.Group name="gender">
+                    <Radio value={1}>Male</Radio>
+                    <Radio value={2}>Female</Radio>
+                  </Radio.Group>
+                </Form.Item>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col sm={8}>
+                <Form.Item
+                  name="address"
+                  label="Address"
+                  labelCol={{ span: 6 }}
+                  wrapperCol={{ span: 16 }}
+                >
+                  <Input name="address" type="text" />
+                </Form.Item>
+              </Col>
+              <Col sm={8}>
+                <Form.Item
+                  name="degree"
+                  label="Degree"
+                  labelCol={{ span: 6 }}
+                  wrapperCol={{ span: 16 }}
+                >
+                  <Select
+                    style={{
+                      width: 120,
+                    }}
+                    name="degree"
+                    options={degreeOption}
+                  ></Select>
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row>
+              <Col lg={16}>
+                <Upload
+                  name="excel"
+                  customRequest={dummyRequest}
+                  onChange={(e) => handleupload(e, setFieldValue)}
+                  accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                >
+                  <button type="button">Click to Upload</button>
+                </Upload>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={8}>
+                <Form.Item
+                  name="phno"
+                  label="Phone No"
+                  labelCol={{ span: 6 }}
+                  wrapperCol={{ span: 16 }}
+                >
+                  <Input name="phno" />
+                </Form.Item>
+              </Col>
+              <Col>
+                <SubmitButton>Submit</SubmitButton>
+              </Col>
+            </Row>
+            <Debug />
+          </Form>
+        );
+      }}
     </Formik>
   );
 }
